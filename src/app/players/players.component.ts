035f2f2bef59c13f2511fa6d67c80c0b1626e7ee
Jian Li <Jian.Li-4@student.uts.edu.au>
2018-10-06 17:11:12 +1000

    Update players page layout

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Player } from '../models/player.model';
 
 @Component({
   selector: 'aip-players',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersComponent implements OnInit {
 
-  constructor() { }
+  public teamName: string;
+  public players: Player[];
+  public hasPlayer: boolean;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.teamName = params['teamName'];
+      }
+    );
+
+    this.players = [
+      {name: 'TestingA'},
+      {name: 'TestingB'},
+      {name: 'TestingC'},
+      {name: 'TestingD'},
+    ];
+
+    this.hasPlayer = this.players && this.players.length > 0;
   }
 
 }

638ad0da1930838498a594efe7f302559b4b8cc0
Jian Li <Jian.Li-4@student.uts.edu.au>
2018-10-06 14:26:05 +1000

    Update teams page layout

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Team } from '../models/team.model';
 
 @Component({
   selector: 'aip-teams',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamsComponent implements OnInit {
 
+  public teams: Team[];
+  public hasTeam: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.teams = [
+      { name: 'Dynama', players: [{name: 'ABCD'}] },
+      { name: 'Dr IQ', players: [{name: 'ABCD'}, {name: 'ABCD'}] },
+      { name: 'Magma', players: [] },
+      { name: 'Tornado', players: [{name: 'ABCD'}, {name: 'ABCD'}, {name: 'ABCD'}] }
+    ];
+
+    this.hasTeam = this.teams && this.teams.length > 0;
   }
+
+  public createClick(): void {
+    this.teams.push({ name: 'Test', players: [] });
+  }
+
 
 }

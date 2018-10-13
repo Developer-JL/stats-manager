26c368e665132a62b68f9ab781e20801315c885b
Jian Li <Jian.Li-4@student.uts.edu.au>
2018-10-13 21:07:05 +1100

    Update sign in and sign out function

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthGuardService } from '../../services/auth-guard/auth-guard.service';
+import { TeamService } from '../../services/team/team.service';
 
 @Component({
   selector: 'aip-header',
@@ -8,13 +10,26 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  public isLoggedIn: any;
+
+  constructor(private router: Router, private some: AuthGuardService, private service: TeamService) { }
 
   ngOnInit() {
+    this.service.change.subscribe(isOpen => {
+      this.isLoggedIn = isOpen;
+    });
+;
   }
 
   public signInClick(): void {
     this.router.navigate(['/sign-in']);
+  }
+
+  public signOutClick(): void {
+    localStorage.removeItem('userId');
+    // this.isLoggedIn = false;
+    this.service.toggle();
+    this.router.navigate(['/']);
   }
 
 }

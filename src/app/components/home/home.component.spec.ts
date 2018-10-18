31ba25f00ada78a17397ff7858bceaf21cff010f
Jian Li <Jian.Li-4@student.uts.edu.au>
2018-10-18 22:02:25 +1100

    Update unit test for HomeComponent

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,7 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -23,7 +24,24 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
+  let router: Router;
+
+  beforeEach(inject(
+    [],
+    () => {
+      router = TestBed.get(Router);
+    }
+  ));
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should navigate to sign up page`, () => {
+    spyOn(router, 'navigate');
+    component.signUpClick();
+    expect((router.navigate as any).calls.mostRecent().args[0]).toEqual([
+      '/sign-up',
+    ]);
   });
 });
